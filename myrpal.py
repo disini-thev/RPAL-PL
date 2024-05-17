330c60fe1dba152fe5ef5bfd89af8c56ff7abe15
disini-thev <ma17disini@gmail.com>
2024-05-18 00:02:21 +0530

    added the ast switch

--- myrpal.py
+++ myrpal.py
@@ -1,94 +1,38 @@
 import sys
-# from stack import Stack
+import argparse
 from tree import Tree
 from parser_ import Parser
 from lexical_analyser_ import LexicalAnalyser
 from Cse_Machine import CSE_Machine
 
 def main():
-    prog_file = sys.argv[1]
+    parser = argparse.ArgumentParser(description='RPAL Interpreter')
+    parser.add_argument('prog_file', type=str, help='the RPAL program file')
+    parser.add_argument('-ast', action='store_true', help='print the abstract syntax tree and exit')
     
-    # print("SCANNING AND SCREENING")
+    args = parser.parse_args()
+    prog_file = args.prog_file
+    
+    # Scanning and screening
     LE = LexicalAnalyser(prog_file)
     tokens = LE.lexical_analyser()
-    # for token in tokens:
-    #     print(token[0], token[1])
     
-    # print("PARSING")
+    # Parsing
     P = Parser(tokens)
-    # P.stripDel()
     P.E()
     AST = Tree.node_stack.top()
-    print("Abstract Syntax Tree")
-    Tree.print_AST(AST)
+    
+    if args.ast:
+        print("Abstract Syntax Tree")
+        Tree.print_AST(AST)
+        return
 
-    with open("..\\Rpal-Interpreter\\AST_output.txt", "w") as file:
-        file.write(Tree.ST_to_string(AST))
-
-    # standardizing the tree
+    # Standardizing the tree
     ST = Tree.standardize_tree(AST)
-    # Tree.print_AST(ST)
-
-
-
-    print("CSE Machine", end="\t")
-
+    
+    # Running the CSE Machine
     machine = CSE_Machine()
     machine.run(ST)
-    
 
 if __name__ == "__main__":
     main()
-
-
-# prog_file = sys.argv[1]
-    
-# # print("SCANNING AND SCREENING")
-# LE = LexicalAnalyser(prog_file)
-# tokens = LE.lexical_analyser()
-# # for token in tokens:
-# #     print(token[0], token[1])
-
-# # print("PARSING")
-# P = Parser(tokens)
-# # P.stripDel()
-# P.E()
-# AST = Tree.node_stack.top()
-# Tree.print_AST(AST)
-# # write the output of print_AST to a file
-# with open("..\\Rpal-Interpreter\\AST_output.txt", "w") as file:
-#     file.write(Tree.ST_to_string(AST))
-    
-
-# # standardizing the tree
-# ST = Tree.standardize_tree(AST)
-
-# print("\n\n\nStandardized Tree\n\n\n")
-# Tree.print_AST(ST)
-
-
-
-# print("\n\n\nStarting the CSE Machine\n\n\n")
-
-
-# controlStructures = []   
-# count = 0
-
-# generateControlStructure(ST,0) ############################
-
-# builtInFunctions = ["Order", "Print", "print", "Conc", "Stern", "Stem", "Isinteger", "Istruthvalue", "Isstring", "Istuple", "Isfunction"]
-
-# control = []
-# stack = []
-# environments = [EnvironmentNode(0, None)]
-# currentEnvironment = 0
-
-# control.append(environments[0].name)
-# control += controlStructures[0]
-
-# stack.append(environments[0].name)
-
-# applyRules()
-
-# print("Output of the above program is:")
-# print(stack[0])
